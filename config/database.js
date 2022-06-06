03076b9e2fb83cfb7a27ac795299ff0e3c94cb7b
Brittany Carter <102538933+codingbycarter@users.noreply.github.com>
2022-06-05 23:35:29 -0400

    Add files via upload
    updated files, install nodemon

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+// exporting whole object with a url and dbname property
+module.exports = {
+    url: 'mongodb+srv://test:72xQhEMu7Qhsj2JY@cluster0.nb8ub8w.mongodb.net/?retryWrites=true&w=majority',
+    'dbName': 'aydenscorner'
+};
+// add db url
